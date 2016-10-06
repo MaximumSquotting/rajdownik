9b0660c7609a2705600cbf1e1968a9d146e7dd36
xxl93 <bartek.blazik@gmail.com>
2016-10-06 12:56:04 +0200

    CRUD done, bootstrap installed

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ gem 'therubyracer', platforms: :ruby
 gem 'jquery-rails'
 gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
+gem 'bootstrap-sass', '~> 3.3.6'
 
 group :development, :test do
   gem 'pry'
@@ -24,6 +25,7 @@ group :development, :test do
   gem 'listen', '~> 3.0.5'
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+  gem 'shoulda-matchers', '~> 3.1'
 end
 
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
---
+++ db/migrate/20161004221208_create_registrations.rb
@@ -0,0 +1,18 @@
+class CreateRegistrations < ActiveRecord::Migration[5.0]
+  def change
+    create_table :registrations do |t|
+      t.string :name, null: false
+      t.string :student_id, null: false
+      t.boolean :accepted, null: false, default: false
+      t.boolean :paid, null: false, default: false
+      t.boolean :attended, null: false, default: false
+      t.string :shirt_size, null: false
+      t.boolean :female_shirt, null: false, default: false
+      t.string :phone_number, null: false
+      t.string :departure_date, null: false
+      t.string :arrival_date, null: false
+
+      t.timestamps
+    end
+  end
+end
