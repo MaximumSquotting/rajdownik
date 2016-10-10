a036247fe20f2bf60c9902fda1e69fbe92487394
Michał Zając <michal.zajac@gmail.com>
2016-10-10 23:37:03 +0200

    Make sure phone number is actually a number

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -1,6 +1,7 @@
 class Registration < ApplicationRecord
   validates :name, :student_id, :shirt_size,
-   :phone_number, :departure_date, :arrival_date, presence: true
+   :departure_date, :arrival_date, presence: true
+  validates :phone_number, presence: true, numericality: { only_integer: true }
   validates :student_id, uniqueness: { scope: :rally, message: I18n.t("errors.student_id_not_unique") }
   belongs_to :rally
 end
