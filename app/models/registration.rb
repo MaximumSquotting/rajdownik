025b23cec20955a9fe919949128428df0476137b
xxl93 <bartek.blazik@gmail.com>
2016-10-11 00:47:08 +0200

    Add surname to form

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -1,5 +1,5 @@
 class Registration < ApplicationRecord
-  validates :name, :student_id, :shirt_size,
+  validates :name, :surname, :student_id, :shirt_size,
    :departure_date, :arrival_date, presence: true
   validates :phone_number, presence: true, numericality: { only_integer: true }
   validates :student_id, uniqueness: { scope: :rally, message: I18n.t("errors.student_id_not_unique") }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161010192307) do
+ActiveRecord::Schema.define(version: 20161010220115) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20161010192307) do
     t.string   "end_date",        null: false
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "student_ids"
   end
 
   create_table "registrations", force: :cascade do |t|
@@ -47,6 +48,7 @@ ActiveRecord::Schema.define(version: 20161010192307) do
     t.datetime "created_at",                            null: false
     t.datetime "updated_at",                            null: false
     t.integer  "rally_id"
+    t.string   "surname"                                null: false
     t.index ["rally_id"], name: "index_registrations_on_rally_id", using: :btree
   end
 
