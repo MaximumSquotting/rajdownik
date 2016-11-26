18d8d0e270538810ff7d7e72528d03ade5edf9af
xxl93 <bartek.blazik@gmail.com>
2016-11-26 01:41:08 +0100

    add signed declaration changed to true

--- app/controllers/confirm_controller.rb
+++ app/controllers/confirm_controller.rb
@@ -3,6 +3,7 @@ class ConfirmController < ApplicationController
     @registration = Registration.where(params[:student_id])
     @registration.last.accepted == 'true'
     @registration.last.accepted_terms == 'true'
+    @registration.last.signed_declaration == 'true'
     @registration.last.save
   end
 end
