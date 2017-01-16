42255adca280b5f2f30d5b32c862fc000a41f234
Michał Zając <michal.zajac@gmail.com>
2017-01-16 15:48:18 +0100

    The index number is the second column

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -1,10 +1,11 @@
 class Registration < ApplicationRecord
   def check_id
+    binding.pry
     found = false
     s = SimpleSpreadsheet::Workbook.read(rally.student_ids.path)
     s.selected_sheet = s.sheets.first
     s.first_row.upto(s.last_row) do |line|
-      known_id = s.cell(line, 1)[-6..-1]
+      known_id = s.cell(line, 2)[-6..-1]
       if known_id == student_id
         found = true
         break
